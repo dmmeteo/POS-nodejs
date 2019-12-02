9cf85b3a1b9e43443d404772097169fb51ca5876
Serhii Ihnatiev <3162695+ihntv@users.noreply.github.com>
2019-12-03 01:11:23 +0200

    Add environment variables.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 
+// Load environment variables
+require('dotenv').config();
+
 const app = express();
 
-app.listen(5000, () => {
-  console.log('app running');
+if (process.env.NODE_ENV === 'production') {
+  // TODO
+  console.log('Production mode enabled!');
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
 });
